6080fb3946a7f5f78939ffea569255c947d94c49
pipe473 <bedoyafelipe743@gmail.com>
2020-10-17 23:51:26 +0200

    Enrutando el chat

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
