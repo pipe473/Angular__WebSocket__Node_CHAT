04a15d08dabbc54594800e709ead1e73d2e91f08
pipe473 <bedoyafelipe743@gmail.com>
2020-10-18 00:23:31 +0200

    Creando la comunicacion con el routerlink y creando un objeto user para añadir atributos

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  User = {
+    name: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
