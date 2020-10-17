d63147945e3f4df16a3910d97939716a2f1a072d
pipe473 <bedoyafelipe743@gmail.com>
2020-10-17 22:01:26 +0200

    Montando los eventos de socket para pasra mensaje

--- server/index.js
+++ server/index.js
@@ -2,6 +2,24 @@
 
 const app = require("express")();
 const serverHTTP = require('http').Server(app)
-const io = require('socket.io')(serverHTTP)
+const io = require('socket.io')(serverHTTP);
+
+const messages = [];
+
+io.on('connection', function (err, socket) {
+    if (err) {
+        console.log(err);        
+    } else {
+        socket.on('send-message', function (err, data) {
+            if (err) {
+                console.log(err);                
+            } else {
+                messages.push(data)
+                socket.emit('text-event', messages)
+                socket.broadcast.emit('text-event', messages)
+            }
+        })
+    }
+})
 
 
