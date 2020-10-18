7f2c80471e36b6e8644ac8ee29c470a184e88a87
pipe473 <bedoyafelipe743@gmail.com>
2020-10-18 18:53:13 +0200

    inicializando la conexion del usuario con socket.io

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
+// use express
 const express = require("express");
+
+// create instance of express
 const app = express();
+
+// use http with instance of express
 const http = require("http").createServer(app)
+
+// create socket instance with http
+const io = require('socket.io')(http);
+
+// add listener for new connection 
+io.on("connection", (socket) =>{
+// this is socket for each user
+console.log("User connected", socket.id);
+
+});
 // const serverHTTP = require('http').Server(app)
 // const io = require('socket.io')(serverHTTP);
 
@@ -25,6 +40,7 @@ app.get("/", (request, result) =>{
 //         });
 //     }
 // });
+
 
 const PORT = 1750;
 
